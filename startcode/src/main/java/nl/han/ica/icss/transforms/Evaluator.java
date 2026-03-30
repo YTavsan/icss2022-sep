5f5fd8275cccf4767da4f6e556d0506ac949937a
Yasin <y.tavsan@student.han.nl>
2026-03-30 20:50:25 +0200

    chore: begin evaluator

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,5 +1,6 @@
 package nl.han.ica.icss.transforms;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.PercentageLiteral;
@@ -17,14 +18,54 @@ public class Evaluator implements Transform {
     private IHANLinkedList<HashMap<String, Literal>> variableValues;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new HANLinkedList<>();
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new HANLinkedList<>();
+        evaluate(ast.root);
 
     }
 
-    
+    private void evaluate(Stylesheet stylesheet) {
+        variableValues.addFirst(new HashMap<>());
+        for (ASTNode childNode : stylesheet.getChildren()) {
+            if (childNode instanceof VariableAssignment) {
+                evaluateVariableAssignment((VariableAssignment) childNode);
+            } else if (childNode instanceof Stylerule) {
+                variableValues.addFirst(new HashMap<>());
+                evaluateStylerule((Stylerule) childNode);
+                variableValues.removeFirst();
+            }
+        }
+    }
+
+    private void evaluateStylerule(Stylerule stylerule) {
+        for (ASTNode childNode : stylerule.getChildren()) {
+            if (childNode instanceof VariableAssignment) {
+                evaluateVariableAssignment((VariableAssignment) childNode);
+            } else if (childNode instanceof Declaration) {
+                evaluateDeclaration((Declaration) childNode);
+            } else if (childNode instanceof IfClause) {
+                evaluateIfClause((IfClause) childNode);
+            } else if (childNode instanceof ElseClause) {
+                evaluateElseClause((ElseClause) childNode);
+            }
+        }
+    }
+
+    private void evaluateVariableAssignment(VariableAssignment variableAssignment) {
+        if (variableAssignment.expression instanceof Literal) {
+            variableValues.getFirst().put(variableAssignment.name.name, (Literal) variableAssignment.expression);
+        } else if (variableAssignment.expression instanceof VariableReference) {
+            variableAssignment.expression = getVariableInScope(((VariableReference) variableAssignment.expression).name);
+            variableValues.getFirst().put(variableAssignment.name.name, (Literal) variableAssignment.expression);
+        } else if (variableAssignment.expression instanceof Operation) {
+            variableAssignment.expression = getOperationResult((Operation) variableAssignment.expression);
+            variableValues.getFirst().put(variableAssignment.name.name, (Literal) variableAssignment.expression);
+        }
+    }
+
+
 }
